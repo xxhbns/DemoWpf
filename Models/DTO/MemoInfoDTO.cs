900692b6ed66a863479231dbbd8064bc28f4748b
时过当下 <10567081+wlhabcd@user.noreply.gitee.com>
2025-10-20 15:58:36 +0800

    xiugai

---
+++ Models/DTO/MemoInfoDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTO
+{
+    /// <summary>
+    /// 备忘录DTO
+    /// </summary>
+    public class MemoInfoDTO
+    {
+        /// <summary>
+        /// 备忘录Id
+        /// </summary>
+        public int MemoId { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int Status { get; set; }
+    }
+}
---
+++ Models/DTO/WaitInfoDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTO
+{
+    /// <summary>
+    /// 待办事项DTO
+    /// </summary>
+    public class WaitInfoDTO
+    {
+        /// <summary>
+        /// 待办事项Id
+        /// </summary>
+        public int WaitId { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int Status { get; set; }
+    }
+}
